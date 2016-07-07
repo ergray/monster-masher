b117bcff60c0369af83b6983ad9fb7f692dea044
Eric Gray <ericxgray@gmail.com>
2016-07-06 18:59:20 -0700

    Changing clickThrough event function

--- app/main-view/main-view.js
+++ app/main-view/main-view.js
@@ -11,15 +11,29 @@ angular.module('monsterMasher.main-view', ['ngRoute'])
 
 .controller('MainViewCtrl', ['$scope', function($scope) {
 	$scope.top = [
+		{counter: 1},
 		'blue',
 		'green'
 	],
 	$scope.middle = [
+		{counter: 1},
 		'yellow',
 		'orange'
 	],
 	$scope.bottom = [
+		{counter: 1},
 		'black',
 		'white'
-	]
+	],
+	$scope.clickThrough = function($event){
+		var tarRow = $event.srcElement.id;
+		var tarDir = $event.srcElement.innerText;
+		console.log(tarRow)
+		//regular expression to extract text before Row
+		//use variable to choose the correct scope array
+
+		//move left or right based on RegEx extract of innerText
+		//move counter back or forth
+		//if you are at either end of the array, auto set counter to other end
+	}
 }]);
